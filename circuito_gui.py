65d57dddfffa513d2445aba3648dc6fb8397da55
Daniel <dobindad@protonmail.com>
2022-06-26 15:05:52 +0100

    update2

--- circuito_gui.py
+++ circuito_gui.py
@@ -1,1 +1,19 @@
-import tkinter
+from tkinter import *
+
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+
+# Inicializar o GUI tkinter
+root = Tk()
+app = Window(root)
+
+# Dimensões default
+root.geometry('600x380')
+
+# Definir titulo da janela
+root.wm_title("Design circuito")
+
+# show window
+root.mainloop()
