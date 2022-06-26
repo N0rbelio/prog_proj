191306a76c1176d86e343b39b64e2f07d61674c4
BuildTools <unconfigured@null.spigotmc.org>
2022-06-26 15:17:56 +0100

    dasda

---
+++ test_not.py
@@ -0,0 +1,22 @@
+from difflib import Differ
+from ast import Compare
+from cgi import print_arguments
+import json
+from operator import truediv
+
+a = open ("file.json")
+file=json.load(a)
+for i in file ["dados"]:
+    print (i)
+a.close()
+
+
+for i in range(1, 5):
+    if file:'opt'== 'NOT' 
+    print(i)
+    print(j)
+
+    if file.opt1()== True:
+        file.resultado2== False
+    elif file.opt1()== False:
+        file.resultado2== True
