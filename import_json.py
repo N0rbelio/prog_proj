b8f77208e9b6eddb879dd9738b6334d7e3cd175d
BuildTools <unconfigured@null.spigotmc.org>
2022-06-28 21:52:25 +0100

    FINAL UPDATE I GUESS

--- import_json.py
+++ import_json.py
@@ -1,142 +1,57 @@
 from cgi import print_form
 import json
-with open('file.json') as f:
-    file = json.load(f)
+with open('file_feed.json') as f: #abre o ficheiro OR
+    file = json.load(f) #define a variavel file para chamar mais tarde o ficheiro 
 
-with open('file_AND.json') as f:
-    file_AND = json.load(f)
+with open('file_AND.json') as f: #abre o ficheiro OR
+    file_AND = json.load(f) #define a variavel file_AND para chamar mais tarde o ficheiro 
 
-with open('file_NOT.json') as f:
-    file_NOT = json.load(f)
+with open('file_NOT.json') as f: #abre o ficheiro OR
+    file_NOT = json.load(f) #define a variavel file_NOT para chamar mais tarde o ficheiro 
 
- #if file.opt1()file.opt2 == file_AND.opt1, file_AND.opt2) 
-print(file)
-print(file["dados"])
+with open('file_OR.json') as f: #abre o ficheiro OR
+    file_OR = json.load(f) #define a variavel file_OR para chamar mais tarde o ficheiro 
 
 for plogica in file["dados"]: #faz o ciclo da porta_logica que se encontra no dicionario dados do ficheiro file.json 
-   # print(plogica)
 
 ######################################################
 #################        AND         #################
 ######################################################
     if plogica["op"] == "AND": #se a porta_logica "op" for igual a AND 
         for pAND in file_AND["AND"]: # entao vamos fazer o ciclo da porta_AND que se encontra no dicionario AND do ficheiro file_AND.json 
-            if (plogica['opt1'] == pAND["opt1"]) and (plogica['opt2'] == pAND["opt2"]):
-                res1=pAND["resul"]
-                print(res1)
-                plogica["resultado"]=res1
-            with open('file.json', 'w') as f:
-                json.dump(file, f)
+            if (plogica['opt1'] == pAND["opt1"]) and (plogica['opt2'] == pAND["opt2"]): #verifica se o opt1 da porta logica é igual á opt1 do AND e verifica se o opt2 da porta logica é igual á opt2 do AND
+                res1=pAND["resul"]  #vai guardar o resultado nesta variavel temporaria 
+                print(res1) #printa na consola o valor guardado
+                plogica["resultado"]=res1 #envia o resultado da variavel temporaria para a variavel resultado do ficheiro file_feed
+            with open('file_feed.json', 'w') as f: #abre o ficheiro 
+                json.dump(file, f) #faz upload das info para o ficheiro 
 
 ######################################################
 #################        OR         ##################
 ######################################################
-
+    if plogica["op"] == "OR": #se a porta_logica "op" for igual a OR 
+        for pOR in file_OR["OR"]: # entao vamos fazer o ciclo da porta_AND que se encontra no dicionario AND do ficheiro file_OR.json 
+            if (plogica['opt1'] == pOR["opt1"]) and (plogica['opt2'] == pOR["opt2"]): #verifica se o opt1 da porta logica é igual á opt1 do OR e verifica se o opt2 da porta logica é igual á opt2 do OR
+                res2=pOR["result"] #vai guardar o resultado nesta variavel temporaria 
+                print(res2) #printa na consola o valor guardado
+                plogica["resultado"]=res2 #envia o resultado da variavel temporaria para a variavel resultado do ficheiro file_feed
+            with open('file_feed.json', 'w') as f: #abre o ficheiro 
+                json.dump(file, f) #faz upload das info para o ficheiro 
 
 
 ######################################################
 #################        NOT         #################
 ######################################################
 
-    elif plogica["op"] == "NOT":
-        for pNOT in file_NOT["NOT"]:
-                if plogica['opt1'] == pNOT["opt1"]:
-                    res3int=pNOT["res"]
-                    
-                    print(res3)
-
-                    plogica["resultado"]=res3
-                with open('file.json', 'w') as f:
-                    json.dump(file, f)
-
-quit()
-#for plogica in file["dados"]: #faz o ciclo da porta_logica que se encontra no dicionario dados do ficheiro file.json 
-   # print(plogica)
-if plogica["op"] == "AND": #se a porta_logica "op" for igual a AND 
-    for pAND in file_AND["AND"]: # entao vamos fazer o ciclo da porta_AND que se encontra no dicionario AND do ficheiro file_AND.json 
-        if (plogica['opt1'] == pAND["opt1"]) and (plogica['opt2'] == pAND["opt2"]):
-            res=pAND["resultado"]
-
-
-
-
-            #plogica['opt1']= plogica['opt2'] and plogica['resultado0'].replace('',pAND['resultado'])
-quit()
-
-
-
-
-
-with open('file.json', 'w') as f:
-    json.dump(file, f)
-                             
-                             #dados[0]["resultado"] = res
-
-
-
-for plogica in file["dados"]: #faz o ciclo da porta_logica que se encontra no dicionario dados do ficheiro file.json 
-   # print(plogica)
-
-    if plogica["op"] == "AND": #se a porta_logica "op" for igual a AND 
-        for pAND in file_AND["AND"]: # entao vamos fazer o ciclo da porta_AND que se encontra no dicionario AND do ficheiro file_AND.json 
-                print(pAND)
-                if (plogica['opt1'] and pAND["opt1"]) == (plogica['opt2'] and pAND["opt2"]):
-                #if (plogica['opt1'] and plogica["opt2"]) == (pAND['opt1'] and pAND["opt2"]):
-                    plogica["resultado0"].replace("" ,pAND["resultado"])
-
-                    with open('file.json', 'w') as f:
-                            json.dump(file, f)
-                    
-
-
-
-
-
-print()
-print()
-print()
-print(plogica['opt1'])
-print(plogica['opt2'])
-print(pAND['opt1'])
-print(pAND['opt2'])
-print()
-print(plogica['resultado0'])
-print(pAND['resultado'])
-
-
-
-
-
-
-for i in range(1,5):
-    for test in file['op']: 
-        if file["op"] == ["AND"]:
-            for i in range(1,4):
-                for file_AND in file_AND['AND']: 
-
-                    if (file['opt1'] and file["opt2"]) == (file_AND['opt1'] and file_AND["opt2"]):
-                        file["resultado0"].replace("", file_AND["resultado"])
-                        
-
-            
-with open('file.json', 'w') as f:
-    json.dump(file, f)
-
-
-
-# elif file["op"] == file_OR["OR"]:
-#     # load do json OR
-#         o = open ('file_OR.json')
-#         file_OR=json.load(o)
-#         for i in file ['file_OR']:
-#             print (i)
-#         o.close()
-
-# else: 
-#     file["op"] == file_NOT["NOT"]
-#     # load do json NOT
-#     n = open ('file_NOT.json')
-#     file_NOT=json.load(n)
-#     for i in file ['file_NOT']:
-#         print (i)
-#     n.close()
+    if plogica["op"] == "NOT":#se a porta_logica "op" for igual a NOT 
+        for pNOT in file_NOT["NOT"]:# entao vamos fazer o ciclo da porta_NOTque se encontra no dicionario AND do ficheiro file_NOT.json 
+                if plogica['opt1'] == pNOT["opt1"]: #verifica se o opt1 da porta logica é igual á opt1 do not
+                    res3=pNOT["res"] #vai guardar o resultado nesta variavel temporaria 
+                    print(res3)#printa na consola o valor guardado
+                    plogica["resultado"]=res3 #envia o resultado da variavel temporaria para a variavel resultado do ficheiro file_feed
+                    with open('file_feed.json', 'w') as f: #abre o ficheiro 
+                        json.dump(file, f) #faz upload das info para o ficheiro 
+                else:
+                    plogica["resultado"]="false" #envia o resultado para a variavel resultado do ficheiro file_feed
+                    with open('file_feed.json', 'w') as f: #abre o ficheiro 
+                        json.dump(file, f) #faz upload das info para o ficheiro 
