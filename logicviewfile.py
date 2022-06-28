53b0a8e01a7b737d8814999447c066680dfdb796
Daniel <dobindad@protonmail.com>
2022-06-28 23:20:30 +0100

    update

--- logicviewfile.py
+++ logicviewfile.py
@@ -7,30 +7,59 @@ def gates():
     Cop = Canvas(bg ='white', height=340, width=590)
     Cop.grid(row=1, column=0, pady = 5, padx = 5, sticky ='w')
     
+    ##AND Gate
+    posAND1 = -70
+    Cop.create_arc(130+posAND1, 150+posAND1, 180+posAND1, 190+posAND1, start=270, extent=180, fill="white", width=3)
+    Cop.create_rectangle(135+posAND1, 150+posAND1, 155+posAND1, 190+posAND1, fill='white', outline='black', width=3)
+    Cop.create_line(155+posAND1, 152+posAND1, 155+posAND1, 189+posAND1, fill="white", width=3)
+    #saida linhas
+    Cop.create_line(181+posAND1,170+posAND1,200+posAND1,170+posAND1, fill="green", width=3)
+    #entrada linhas
+    Cop.create_line(115+posAND1,160+posAND1,134+posAND1,160+posAND1, fill="green", width=3)
+    Cop.create_line(115+posAND1,180+posAND1,134+posAND1,180+posAND1, fill="green", width=3)
+
     ##OR Gate
     #Cop.create_rectangle(10, 10, 70, 70, fill='white', outline='blue', width=3) 
-    tripontos = [150, 250, 155, 275, 150, 300, 180, 297, 190, 294, 210, 275, 192, 260, 180, 255]
+    posOR1 = -85
+    tripontos = [150+posOR1, 250+posOR1, 155+posOR1, 275+posOR1, 150+posOR1, 300+posOR1, 180+posOR1, 297+posOR1, 190+posOR1, 294+posOR1, 210+posOR1, 275+posOR1, 192+posOR1, 260+posOR1, 180+posOR1, 255+posOR1]
     Cop.create_polygon(tripontos, outline='black', fill='white', width=3)
-    Cop.create_line(210, 275, 229, 275, fill="green", width=3)
+    #saida linha
+    Cop.create_line(210+posOR1, 275+posOR1, 229+posOR1, 275+posOR1, fill="green", width=3)
     #entrada linhas
-    Cop.create_line(132, 265, 152, 265, fill="green", width=3)
-    Cop.create_line(132, 285, 152, 285, fill="green", width=3)
+    Cop.create_line(132+posOR1, 265+posOR1, 152+posOR1, 265+posOR1, fill="green", width=3)
+    Cop.create_line(132+posOR1, 285+posOR1, 152+posOR1, 285+posOR1, fill="green", width=3)
 
     #NOT Gate
     #Na seguinte estrutura [x1, y1, x2, y2 ...]
-    tripontos = [150, 100, 150, 120, 180, 110,]
+    posNOT1 = -10
+    tripontos = [150+posNOT1, 100+posNOT1, 150+posNOT1, 120+posNOT1, 180+posNOT1, 110+posNOT1]
     Cop.create_polygon(tripontos, outline='black', fill='white', width=3)
-    circpontos = [175,105,185,115]
+    circpontos = [175+posNOT1,105+posNOT1,185+posNOT1,115+posNOT1]
     Cop.create_oval(circpontos, outline='black', fill='white', width=3)
-    Cop.create_line(186,110,200,110, fill="green", width=3)
-    Cop.create_line(135,110,149,110, fill="green", width=3)
-    
-    ##AND Gate
-    Cop.create_arc(130, 150, 180, 190, start=270, extent=180, fill="white", width=3)
-    Cop.create_rectangle(135, 150, 155, 190, fill='white', outline='black', width=3)
-    Cop.create_line(155, 152, 155, 189, fill="white", width=3)
+    Cop.create_line(186+posNOT1,110+posNOT1,200+posNOT1,110+posNOT1, fill="green", width=3)
+    Cop.create_line(135+posNOT1,110+posNOT1,149+posNOT1,110+posNOT1, fill="green", width=3)
+
+    ##AND 2 Gate
+    posAND1 = +30
+    Cop.create_arc(130+posAND1, 150+posAND1, 180+posAND1, 190+posAND1, start=270, extent=180, fill="white", width=3)
+    Cop.create_rectangle(135+posAND1, 150+posAND1, 155+posAND1, 190+posAND1, fill='white', outline='black', width=3)
+    Cop.create_line(155+posAND1, 152+posAND1, 155+posAND1, 189+posAND1, fill="white", width=3)
     #saida linhas
-    Cop.create_line(181,170,200,170, fill="green", width=3)
+    Cop.create_line(181+posAND1,170+posAND1,200+posAND1,170+posAND1, fill="green", width=3)
     #entrada linhas
-    Cop.create_line(115,160,134,160, fill="green", width=3)
-    Cop.create_line(115,180,134,180, fill="green", width=3)
+    Cop.create_line(115+posAND1,160+posAND1,134+posAND1,160+posAND1, fill="green", width=3)
+    Cop.create_line(115+posAND1,180+posAND1,134+posAND1,180+posAND1, fill="green", width=3)
+
+    ##OR 2 Gate
+    posOR1 = -120
+    tripontos = [380+posOR1, 250+posOR1, 385+posOR1, 275+posOR1, 380+posOR1, 300+posOR1, 410+posOR1, 297+posOR1, 420+posOR1, 294+posOR1, 440+posOR1, 275+posOR1, 422+posOR1, 260+posOR1, 410+posOR1, 255+posOR1]
+    Cop.create_polygon(tripontos, outline='black', fill='white', width=3)
+    #saida linhas
+    Cop.create_line(440+posOR1, 275+posOR1, 459+posOR1, 275+posOR1, fill="green", width=3)
+    #entrada linhas
+    Cop.create_line(382+posOR1, 265+posOR1, 362+posOR1, 265+posOR1, fill="green", width=3)
+    Cop.create_line(382+posOR1, 285+posOR1, 362+posOR1, 285+posOR1, fill="green", width=3)
+
+    #Ligação ao OR 2 Gate
+    Cop.create_line(200+posNOT1,110+posNOT1,362+posOR1, 265+posOR1, fill="green", width=3)
+    Cop.create_line(200+posAND1,170+posAND1,362+posOR1, 285+posOR1, fill="green", width=3)
